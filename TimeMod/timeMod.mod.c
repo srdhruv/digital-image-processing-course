07676c85a077490c21952c155c4c7b4fc08b10c1
Dhruv Sawarkar <44495832+thatcathatechat@users.noreply.github.com>
2022-04-13 10:11:02 +0530

    Add files via upload

---
+++ TimeMod/timeMod.mod.c
@@ -0,0 +1,37 @@
+#include <linux/build-salt.h>
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+BUILD_SALT;
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__section(.gnu.linkonce.this_module) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+#ifdef CONFIG_RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+static const struct modversion_info ____versions[]
+__used __section(__versions) = {
+	{ 0xb3753869, "module_layout" },
+	{ 0xdecd0b29, "__stack_chk_fail" },
+	{ 0x9ec6ca96, "ktime_get_real_ts64" },
+	{ 0xc5850110, "printk" },
+	{ 0xbdfb6dbb, "__fentry__" },
+};
+
+MODULE_INFO(depends, "");
+
+
+MODULE_INFO(srcversion, "1A9B1C6E0709D7E02204310");
